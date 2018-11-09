7a476c3ae444f7b71a8a4babd9bfc5f8298df863
awanirawat <rawatawani@gmail.com>
2018-11-09 10:31:26 +0530

    register player

--- Game.sol
+++ Game.sol
@@ -1,10 +1,47 @@
 pragma solidity ^0.4.24;
 contract Game{
     int reward;
-    int [][] board;
+    mapping(int=>mapping(int=>int)) board;
+    int turn;
+    int count;
+    struct player
+    {
+        address addr;
+        int balance;
+        int fee;
+        int turn;
+    }
+    //player wins 2/3 of the total fee paid
     function Game()
     {
-        
+        for(int i=0;i<3;i++)
+        {
+            for(int j=0;j<3;j++)
+                board[i][j]=-1;
+        }
+        turn=0;
+        count=0;
     }
-    
+    function register(int f)
+    {
+        if(count==0)
+        {
+            address ad=msg.sender;
+            player p1;
+            p1.addr=ad;
+            p1.fee=f;
+            p1.balance=0;
+            count++;
+            p1.turn=1;
+        }
+        else if(count==1)
+        {
+            address ad=msg.sender;
+            player p1;
+            p1.addr=ad;
+            p1.fee=f;
+            p1.balance=0;
+            count++;
+            p1.turn=1;
+        }
 }
