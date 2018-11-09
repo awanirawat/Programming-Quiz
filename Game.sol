3c8a82088a8ca60934554cf16206f1a168cb3e31
awanirawat <rawatawani@gmail.com>
2018-11-09 11:00:15 +0530

    Declaring winner

--- Game.sol
+++ Game.sol
@@ -4,6 +4,9 @@ contract Game{
     mapping(int=>mapping(int=>int)) board;
     int turn;
     int count;
+    int total_fee;
+    address host;
+    int public winner;
     struct player
     {
         address addr;
@@ -23,10 +26,11 @@ contract Game{
         }
         turn=0;
         count=0;
+        host=msg.sender;winner=0;
     }
     function check_state(int r1,int c1,int r2,int c2,int r3,int c3) returns (int)
     {
-        if(board[r1][c1]==board[r2][c2] && board[r2][c2]==board[r3][c3])
+        if(board[r1][c1]!=-1&&board[r2][c2]!=-1&&board[r1][c1]==board[r2][c2] && board[r2][c2]==board[r3][c3])
             return board[r1][c1];
         else
         return -1;
@@ -34,7 +38,7 @@ contract Game{
     }
     function register(int f)
     {
-        if(count==0)
+        if(count==0&&msg.sender!=host)
         {
             address ad=msg.sender;
             p1.addr=ad;
@@ -42,36 +46,77 @@ contract Game{
             p1.balance=0;
             count++;
             p1.turn=1;
+            total_fee+=f;
         }
-        else if(count==1&&msg.sender!=p1.addr)
+        else if(count==1&&msg.sender!=p1.addr&&msg.sender!=host)
         {
             p2.addr=msg.sender;
             p2.fee=f;
             p2.balance=0;
             count++;
             p2.turn=2;
+            total_fee+=f;
             count++;
         }
     }
-    function check_winner()
+    function check_winner() returns(int)
     {
-        
+      int res=check_state(0,0,1,1,2,2);
+      if(res!=-1)return res;
+      res=check_state(0,2,1,1,2,0); 
+      if(res!=-1)return res;
+      res=check_state(0,0,0,1,0,2);
+      if(res!=-1)return res;
+      res=check_state(1,0,1,1,1,2);
+      if(res!=-1)return res;
+      res=check_state(2,0,2,1,2,2);
+      if(res!=-1)return res;
+      res=check_state(0,0,1,0,2,0);
+      if(res!=-1)return res;
+      res=check_state(0,1,1,1,2,1);
+      if(res!=-1)return res;
+      res=check_state(0,2,1,2,2,2);
+      if(res!=-1)return res;
+      return -1;
+      
+    }
+    function add_reward(address addr)
+    {
+        if(p1.addr==addr)
+            p1.balance=(2*total_fee)/3;
+        else if(p2.addr==addr)
+            p2.balance=(2*total_fee)/3;
+
     }
     function play(int row,int col)
     {
+        if(msg.sender==host&&winner==0){
         if(count==2&&row>=0&&row<3&&col>=0&&col<3&&board[row][col]==-1)
         {
             address a=msg.sender;
             if(p1.addr==a&&turn==1)
             {
                 board[row][col]=0;//0
-                check_winner();
+                int res=check_winner();
+                if(res==board[row][col])
+                {
+                    add_reward(a);
+                    winner=1;
+                }
+                turn=2;
             }
             else if(p2.addr==a&&turn==2)
             {
                 board[row][col]=1;//X
-                check_winner();
+                res=check_winner();
+                if(res==board[row][col])
+                {
+                    add_reward(a);
+                    winner=2;
+                }
+                turn=1;
             }
         }
+        }
     }
-}c
+}
